22630fbeb93856096db045f80a9344de26e01d3c
Masaki SATO <m.sato@exanet.co.jp>
2015-07-25 17:24:56 +0900

    Edit events.rb
    -- Add WebsocketChatController1

--- config/events.rb
+++ config/events.rb
@@ -11,4 +11,6 @@ WebsocketRails::EventMap.describe do
   #     subscribe :new, :to => ProductController, :with_method => :new_product
   #   end
   # The above will handle an event triggered on the client like `product.new`.
+
+  subscribe :websocket_chat, to: WebsocketChatContrller, wiht_method: :message_recieve
 end
